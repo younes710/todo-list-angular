5f9cae0ea4316f67cce5ef0713150ec62cd9097e
Younes Abdi <realyoonesabdi@gmail.com>
2023-07-08 21:07:06 +0330

    Convert starter html to multiple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { FormAddTodoComponent } from './components/form-add-todo/form-add-todo.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormAddTodoComponent,
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form-add-todo/form-add-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-add-todo',
+  templateUrl: './form-add-todo.component.html',
+  styleUrls: ['./form-add-todo.component.css']
+})
+export class FormAddTodoComponent {
+
+}
